1b633c47b2ac3948025bd7b808a2bf385d7e9f23
Mohnish Mirchandani <mirchandani.mohnish03@gmail.com>
2022-06-23 15:22:44 +0530

    Updates: - Blog Component deleted - Minor Fixes - Patch: Titles added for courses

---
+++ docs/schools/sas/conversational-french-1.md
@@ -0,0 +1,1 @@
+# Conversational French - 1
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -69,7 +69,7 @@ const config = {
             position: "left",
             label: "Tutorial",
           },
-          { to: "/blog", label: "Blog", position: "left" },
+
           {
             href: "https://github.com/mirchandani-mohnish/AUdocs",
             label: "GitHub",
@@ -106,10 +106,6 @@ const config = {
           {
             title: "More",
             items: [
-              {
-                label: "Blog",
-                to: "/blog",
-              },
               {
                 label: "GitHub",
                 href: "https://github.com/mirchandani-mohnish/AUdocs",
