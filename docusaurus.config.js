dba9e462a47f0b0efa73b6258feb6df87c483dc7
Mohnish Mirchandani <mirchandani.mohnish03@gmail.com>
2023-04-17 10:53:47 +0530

    Update: editurl changed

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,15 +36,7 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/mirchandani-mohnish/AUdocs/tree/master",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
